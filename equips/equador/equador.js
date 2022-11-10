2d2c788e9cf7a364a792b7ed124ce8393149a3e8
Victor Maciel Oliveira <2001oliveira.victor@gmail.com>
2022-11-09 22:01:35 -0300

    Projeto COPA-DO-MUNDO

--- equips/equador/equador.js
+++ equips/equador/equador.js
@@ -3,15 +3,15 @@ let playBtn = document.getElementById('play');
 let pauseBtn = document.getElementById('pause');
 
 // Play audio & mostra pause btn
-let playShow = function() {
-    hino.play();
-    playBtn.style.display = "none";
-    pauseBtn.style.display = "flex";
-  };
-  
+let playShow = function () {
+  hino.play();
+  playBtn.style.display = "none";
+  pauseBtn.style.display = "flex";
+};
+
 // Pause audio & mostra play btn
-let pauseShow = function() {
-    hino.pause();
-    playBtn.style.display = "flex";
-    pauseBtn.style.display = "none";
-  };
+let pauseShow = function () {
+  hino.pause();
+  playBtn.style.display = "flex";
+  pauseBtn.style.display = "none";
+};
