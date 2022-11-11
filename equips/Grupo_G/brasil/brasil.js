44d3dea34f788cca396f23b907ef60a6ea5ffe54
Victor Maciel Oliveira <2001oliveira.victor@gmail.com>
2022-11-10 22:16:39 -0300

    Ajuste da estrutura do projeto

---
+++ equips/Grupo_G/brasil/brasil.js
@@ -0,0 +1,15 @@
+let hino = document.getElementById('hino');
+let playBtn = document.getElementById('play');
+let pauseBtn = document.getElementById('pause');
+
+let playShow = function () {
+    hino.play();
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "flex";
+};
+
+let pauseShow = function () {
+    hino.pause();
+    playBtn.style.display = "flex";
+    pauseBtn.style.display = "none";
+};
