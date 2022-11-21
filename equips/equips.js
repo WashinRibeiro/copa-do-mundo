fe0a4c4fd55fcfc58dbbf8101db871c22cccfce7
Washington Ribeiro <washinuchiha@gmail.com>
2022-11-21 16:49:19 -0300

    updatee web component

--- equips/equips.js
+++ equips/equips.js
@@ -14,9 +14,15 @@ let pauseShow = function () {
   pauseBtn.style.display = "none";
 };
 
+function selections() {
+  grupoA();
+  grupoB();
+  grupoC();
+  grupoD();
+  grupoE();
+}
 
-class GroupList extends HTMLElement {
-  div = document.createElement("div");
+function grupoA() {
   group_a = [
     {
       name: "Catar",
@@ -40,108 +46,194 @@ class GroupList extends HTMLElement {
     },
   ];
 
-  constructor() {
-    super().attachShadow({ mode: "open" }).append(this.div);
-  }
-
-  connectedCallback() {
-    this.render();
-  }
-
-  render() {
-    this.div.innerHTML = `<style>
-    .group {
-        width: 100%;
-        padding: 20px 5% 50px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 30px;
-        flex-wrap: wrap;
-      }
-      
-      .flag {
-        flex: 1;
-        text-align: center;
-      }
-      
-      .img {
-        width: 100%;
-        max-width: 320px;
-        height: 180px;
-        min-width: 250px;
-        box-shadow: 0px 0px 20px #0000006e;
-        transition: all 0.4s;
-        object-fit: cover;
-        box-shadow: 0px 0px 20px #0000006e;
-        border-radius: 8px;
-      }
-      
-      .img:hover {
-        transform: scale(1.05);
-        transition: all 0.4s;
-      }
-    </style>`
-
-    this.div.innerHTML = this.group_a.reduce(
-      (s, f) =>
-        (s += `<a href="${f.link}" class="flag">
+  window.document.getElementById("grupo_a").innerHTML = this.group_a.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
                 <img
                     src="${f.img}"
                     alt="bandeira ${f.name}"
                 />
             </a>`),
-      ""
-    );
-  }
+    ""
+  );
 }
 
-customElements.define("group-list-a", GroupList);
+function grupoB() {
+  group_b = [
+    {
+      name: "Estados Unidos",
+      link: "./Grupo_B/estados_unidos.html",
+      img: "../assets/images/Grupo_B/estados_unidos/grupoB_estados_unidos.png",
+    },
+    {
+      name: "Inglaterra",
+      link: "./Grupo_B/inglaterra.html",
+      img: "../assets/images/Grupo_B/inglaterra/grupoB_inglaterra.png",
+    },
+    {
+      name: "Irã",
+      link: "./Grupo_B/ira.html",
+      img: "../assets/images/Grupo_B/ira/grupoB_ira.png",
+    },
+    {
+      name: "Pais de Gales",
+      link: "./Grupo_B/pais_de_gales.html",
+      img: "../assets/images/Grupo_B/pais_de_gales/grupoB_pais_de_gales.png",
+    },
+  ];
 
-
-
-
-
-
-
-
-
-
-
-const template = document.createElement("template");
-template.innerHTML = `
-<style>
-
-
-<div class="group">
-    <a> <img/> </a>
-</div>`;
-
-class EmployeeCard extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
-    this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-    this.render();
-    let radiusInput = this.shadow.querySelector('#radius');
-    let colorInput = this.shadow.querySelector('#color');
-    radiusInput.addEventListener('change', this.changeRadius.bind(this));
-    colorInput.addEventListener('change', this.changeColor.bind(this));
-  }
-
-  connectedCallback() {
-    this.h3 = this.getAttribute("name");
-    this.render();
-  }
-
-  render() {
-    this.h3;
-  }
+  window.document.getElementById("grupo_b").innerHTML = this.group_b.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
+                <img
+                    src="${f.img}"
+                    alt="bandeira ${f.name}"
+                />
+            </a>`),
+    ""
+  );
 }
 
-window.customElements.define("employee-card", EmployeeCard);
+function grupoC() {
+  group_c = [
+    {
+      name: "Arábia Saudita",
+      link: "./Grupo_C/arabia_saudita.html",
+      img: "../assets/images/Grupo_C/arabia_saudita/grupoC_arabia_saudita.png",
+    },
+    {
+      name: "Argentina",
+      link: "./Grupo_C/mexico.html",
+      img: "../assets/images/Grupo_C/mexico/grupoC_mexico.jpg",
+    },
+    {
+      name: "Mexico",
+      link: "./Grupo_B/ira.html",
+      img: "../assets/images/Grupo_B/ira/grupoB_ira.png",
+    },
+    {
+      name: "Polonia",
+      link: "./Grupo_C/polonia.html",
+      img: "../assets/images/Grupo_C/polonia/grupoC_polonia.png",
+    },
+  ];
+
+  window.document.getElementById("grupo_c").innerHTML = this.group_c.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
+                <img
+                    src="${f.img}"
+                    alt="bandeira ${f.name}"
+                />
+            </a>`),
+    ""
+  );
+}
+
+function grupoD() {
+  group_d = [
+    {
+      name: "Austrália",
+      link: "./Grupo_D/australia.html",
+      img: "../assets/images/Grupo_D/australia/grupoD_australia.webp",
+    },
+    {
+      name: "Dinamarca",
+      link: "./Grupo_D/dinamarca.html",
+      img: "../assets/images/Grupo_D/dinamarca/grupoD_dinamarca.png",
+    },
+    {
+      name: "França",
+      link: "./Grupo_D/franca.html",
+      img: "../assets/images/Grupo_D/franca/grupoD_franca.png",
+    },
+    {
+      name: "Tunisia",
+      link: "./Grupo_D/tunisia.html",
+      img: "../assets/images/Grupo_D/tunisia/grupoD_tunisia.png",
+    },
+  ];
+
+  window.document.getElementById("grupo_d").innerHTML = this.group_d.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
+                <img
+                    src="${f.img}"
+                    alt="bandeira ${f.name}"
+                />
+            </a>`),
+    ""
+  );
+}
+
+function grupoE() {
+  group_e = [
+    {
+      name: "Alemanha",
+      link: "./Grupo_E/alemanha.html",
+      img: "../assets/images/Grupo_E/alemanha/grupoE_alemanha.png",
+    },
+    {
+      name: "Costa Rica",
+      link: "./Grupo_E/costa_rica.html",
+      img: "../assets/images/Grupo_E/costa_rica/grupoE_costa_rica.png",
+    },
+    {
+      name: "Espanha",
+      link: "./Grupo_E/espanha.html",
+      img: "../assets/images/Grupo_E/espanha/grupoE_espanha.webp",
+    },
+    {
+      name: "Japão",
+      link: "./Grupo_E/japao.html",
+      img: "../assets/images/Grupo_E/japao/grupoE_japao.jpeg",
+    },
+  ];
+
+  window.document.getElementById("grupo_e").innerHTML = this.group_e.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
+                <img
+                    src="${f.img}"
+                    alt="bandeira ${f.name}"
+                />
+            </a>`),
+    ""
+  );
+}
+
+function grupoF() {
+  group_f = [
+    {
+      name: "Brasil",
+      link: "./Grupo_G/brasil.html",
+      img: "../assets/images/Grupo_G/brasil/grupoG_brasil.png",
+    },
+    {
+      name: "Camarões",
+      link: "./Grupo_G/camaroes.html",
+      img: "../assets/images/Grupo_G/camaroes/grupoG_camaroes.png",
+    },
+    {
+      name: "Sérvia",
+      link: "./Grupo_G/servia.html",
+      img: "../assets/images/Grupo_G/servia/grupoG_servia.webp",
+    },
+    {
+      name: "Suíça",
+      link: "./Grupo_G/suica.html",
+      img: "../assets/images/Grupo_G/suica/grupoG_suica.webp",
+    },
+  ];
+
+  window.document.getElementById("grupo_e").innerHTML = this.group_e.reduce(
+    (s, f) =>
+      (s += `<a href="${f.link}" class="flag">
+                <img
+                    src="${f.img}"
+                    alt="bandeira ${f.name}"
+                />
+            </a>`),
+    ""
+  );
+}
